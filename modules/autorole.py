be51e174adc368729c254e53749f17650dcf2c07
Talha Şöhret <46840264+Fenish@users.noreply.github.com>
2022-05-19 02:00:58 +0300

    Update autorole.py

--- modules/autorole.py
+++ modules/autorole.py
@@ -1,1 +1,10 @@
+from discord.ext import commands
 
+
+class AutoRole(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+async def setup(bot):
+    await bot.add_cog(AutoRole(bot))
