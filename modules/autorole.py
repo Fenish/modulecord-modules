4f89b9726c35e671579b10dce8df6034944715e8
Fenish <sohretalhadev@gmail.com>
2022-05-31 11:03:35 +0300

    add cog info

--- modules/autorole.py
+++ modules/autorole.py
@@ -1,6 +1,11 @@
 from discord.ext import commands
 
 
+# Name: Autorole
+# Author: fenish#1703
+# Description: Gives specified role automatically to newcomers on your server
+
+
 class AutoRole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
